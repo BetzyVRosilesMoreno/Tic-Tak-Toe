dc4d1fa3433fab9a819f2203e7b8366539e11632
Betzy V. Rosiles Moreno <brosilesmoreno@bsd220.org>
2023-11-14 09:34:06 -0600

    Initial Commit

---
+++ Tic-Tak-Toe/Tic_Tak_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tak_ToeApp.swift
+//  Tic-Tak-Toe
+//
+//  Created by Betzy Moreno on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tak_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

cba2e969c9d65145885b14313d5df927f59a028e
Betzy V. Rosiles Moreno <brosilesmoreno@bsd220.org>
2023-11-17 09:42:03 -0600

    "Added Flip animation"

--- Tic-Tak-Toe/ContentView.swift
+++ Tic-Tak-Toe/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-   @State private var moves = Array(repeating: "", count: 9)
+    @State private var moves = Array(repeating: "", count: 9)
     @State private var xTurn = true
     var body: some View {
         VStack {
@@ -17,34 +17,38 @@ struct ContentView: View {
                 .fontWeight(.bold)
                 .preferredColorScheme(.dark)
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 15), count: 3), spacing: 15) {
-                    
+                
                 ForEach(0..<9) { index in
                     ZStack {
                         Color.blue
+                        Color.white
+                            .opacity(moves[index] == "" ? 1 : 0)
                         Text(moves[index])
                             .font(.system(size: 90))
                             .fontWeight(.heavy)
                     }
-                        .frame(width: 120, height: 120, alignment: .center)
-                        .cornerRadius(30)
-                        .onTapGesture {
-                            withAnimation(.default) {
-                                if moves[index] == "" {
-                                    moves[index] = xTurn ? "X":"O"
-                                    xTurn.toggle()
-                                }
+                    .frame(width: 120, height: 120, alignment: .center)
+                    .cornerRadius(30)
+                    .onTapGesture {
+                        withAnimation(.default) {
+                            if moves[index] == "" {
+                                moves[index] = xTurn ? "X":"O"
+                                xTurn.toggle()
                             }
                         }
-                }
+                    }
+                    .rotation3DEffect(.init(degrees: moves[index] != "" ? 180 : 0),
+                                      axis: (x: 0.0, y: 1.0, z: 0.0))
                 }
             }
-            .padding()
         }
+        .padding()
     }
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
+}
 
